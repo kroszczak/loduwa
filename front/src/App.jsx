7d8df27d2db6426f3779aa42627bca08bec61d45
juliaume <juliaum@onet.pl>
2025-01-28 11:28:34 +0100

    dodawanie obiektów w losowym miejscu

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,40 +1,74 @@
-import { Component } from 'react';
-import { Stage, Layer, Text } from 'react-konva';
+import React, { Component } from "react";
+import { Stage, Layer, Image } from "react-konva";
+import { IconButton } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+
+const loadImage = (src) => {
+  const img = new window.Image();
+  img.src = src;
+  return img;
+};
 
 class App extends Component {
   state = {
-    isDragging: false,
-    x: 50,
-    y: 50,
+    objects: [],
+    imageSrc: loadImage("kaczka.jpg"),
+  };
+
+  addObject = () => {
+    const newObject = {
+      x: Math.random() * window.innerWidth,
+      y: Math.random() * window.innerHeight,
+    };
+    this.setState((prevState) => ({
+      objects: [...prevState.objects, newObject],
+    }));
+  };
+
+  handleDragEnd = (e, index) => {
+    const newObjects = [...this.state.objects];
+    newObjects[index] = {
+      ...newObjects[index],
+      x: e.target.x(),
+      y: e.target.y(),
+    };
+    this.setState({ objects: newObjects });
   };
 
   render() {
     return (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Text
-            text="Draggable Text"
-            
-            x={this.state.x}
-            y={this.state.y}
-            draggable
-            fill={this.state.isDragging ? 'green' : 'black'}
-            onDragStart={() => {
-              this.setState({
-                isDragging: true,
-              });
-            }}
+      <div>
+        {/* Chakra UI przycisk */}
+        <IconButton
+          icon={<AddIcon />}
+          aria-label="Dodaj obiekt"
+          onClick={this.addObject}
+          position="absolute"
+          top="20px"
+          right="20px"
+          zIndex="10"
+          colorScheme="red"
+          size="lg"
+          isRound
+        />
 
-            onDragEnd={(e) => {
-              this.setState({
-                isDragging: false,
-                x: e.target.x(),
-                y: e.target.y(),
-              });
-            }}
-          />
-        </Layer>
-      </Stage>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            {this.state.objects.map((obj, index) => (
+              <Image
+                key={index}
+                x={obj.x}
+                y={obj.y}
+                draggable
+                image={this.state.imageSrc}
+                onDragEnd={(e) => this.handleDragEnd(e, index)}
+                width={50}
+                height={50}
+              />
+            ))}
+          </Layer>
+        </Stage>
+      </div>
     );
   }
 }
